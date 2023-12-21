3389883623d1665dc96fe653602509bbe3c8e96d
AlanJuker <alan4630@gmail.com>
2023-12-21 11:49:36 -0500

    .

--- src/components/Home/Carousel/Carousel.jsx
+++ src/components/Home/Carousel/Carousel.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import './Carousel.css'; // Importa el archivo de estilos
-import { ImagesProvider, useImagesApi } from '../../../api/useImagesApi';
+import './Carousel.css'; 
 
 const Carousel = ({ carouselImages }) => {
   const settings = {
